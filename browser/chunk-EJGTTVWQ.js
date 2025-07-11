a256e7f42385a752edc06a0d008832abbabc1c8c
matheusbalduino <matheus.balduin@gmail.com>
2025-07-11 22:47:31 +0200

    Auto-generated commit

---
+++ browser/chunk-EJGTTVWQ.js
@@ -0,0 +1,1 @@
+import{i as a}from"./chunk-TXEKV7TR.js";import"./chunk-LKV5R2YV.js";import"./chunk-Q64L5WXK.js";export{a as HomeComponent};
---
+++ browser/chunk-NXAQE2RL.js
@@ -0,0 +1,24 @@
+import{a as d}from"./chunk-5RVKKZCV.js";import"./chunk-MRM6Q25Q.js";import"./chunk-TXEKV7TR.js";import"./chunk-LKV5R2YV.js";import{Da as i,Ta as o,Xa as a,Ya as r,Za as s,ba as n,nb as l}from"./chunk-Q64L5WXK.js";var f=(()=>{class e{constructor(){this.items=[{title:"Software Engineer",subtitle:"Freelance, Remote",content:`Freelance web developer focused on building and enhancing websites with
+                modern technologies. In charge of designing and delivering features following
+                agile best practices. 
+                Proficient in automating deployments through CI/CD
+                pipelines with GitLab. Well-versed in TypeScript, C#, .NET Core 8, and
+                JavaScript.`,date:"APRIL 2025 - PRESENT"},{title:"Full-Stack Developer",subtitle:"SERPRO, Remote",content:`FullStack developer working for the government of Brazil, developing and
+                improving websites using Node.js technologies, .Net technologies, Angular and
+                PostgreSQL. Responsible for designing and implementing features with Scrum
+                methodology. Experienced in deployment automation with CI/CD pipelines
+                using GitLab. Skilled in TypeScript, C# and .NET Core 8.`,date:"JUNE 2024 - APRIL 2025"},{title:"Full-Stack Developer",subtitle:"Laqus, Remote",content:`Collaborating on API development and client integration using REST API and
+                microservices technologies in a fintech company that issues stocks.
+                Participating in the creation of a platform from scratch with Angular
+                (microfrontend) and maintaining a website in React for stock issuance.
+                Teamwork, Code Review, Gitflow, deploy and scrum methodology.
+                Utilizing NestJS and .Net Core.`,date:"SEPTEMBER 2022 - JUNE 2024"},{title:"Full-Stack NodeJs",subtitle:"TechSavvy, Brazil",content:`Worked as back-end developer programming features for an fintech company, 
+                responsible for e-commerce gateway, POS and Billets. Integrating systems
+                and refact code with the SOLID principles in Rest APIs based on NodeJs
+                technologies. Working as a team with gitflow in a young and engaging
+                company. NodeJS, Adonis, VueJs, React and Angular.`,date:"SEPTEMBER 2022 - JUNE 2024"},{title:"Full-Stack Developer",subtitle:"Unimed, Brazil",content:`Web system development for Unimed Company. System integration with REST
+                APIs, development of Windows services and website development for
+                customers and internal tasks. (.net pdf creation, upload and download of files,
+                sql searches and responsive websites). C# .Net Core, Angular.`,date:"JANUARY 2021 - DECEMBER 2021"},{title:"Front-End Developer",subtitle:"Garden, Brazil",content:`JavaScript Backend web system development for Wix platform. Knowledge of
+                Wix Velo API Development for advanced Wix Websites and REST API Website
+                Integration.<br/><br/>`,date:"SEPTEMBER 2020 - JANUARY 2021"}]}static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["app-experience"]],standalone:!0,features:[l],decls:2,vars:1,consts:[[3,"items"]],template:function(t,p){t&1&&(a(0,"div"),s(1,"app-grid-context",0),r()),t&2&&(i(),o("items",p.items))},dependencies:[d]})}}return e})();export{f as ExperienceComponent};
